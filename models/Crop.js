97bb16f1c711dd832587b7750c423dbe8fc62b7c
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-09 16:47:05 +0530

    Update crop and expense model

--- models/Crop.js
+++ models/Crop.js
@@ -1,3 +1,5 @@
+const mongoose = require('mongoose');
+
 const cropSchema = new mongoose.Schema({
   farmer: {
     type: mongoose.Schema.Types.ObjectId,
--- models/Expense.js
+++ models/Expense.js
@@ -1,3 +1,5 @@
+const mongoose = require("mongoose");
+
 const expenseSchema = new mongoose.Schema({
   farmer: {
     type: mongoose.Schema.Types.ObjectId,
