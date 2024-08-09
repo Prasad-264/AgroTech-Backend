e4d9d7b4bb0b6fa37ae6bbb680fd37ce28049845
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-09 15:35:42 +0530

    Added Expense model

---
+++ models/Expense.js
@@ -0,0 +1,27 @@
+const expenseSchema = new mongoose.Schema({
+  farmer: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: "Farmer",
+    required: true,
+  },
+  crop: { 
+    type: mongoose.Schema.Types.ObjectId, 
+    ref: "Crop", 
+    required: true 
+  },
+  expenseType: { 
+    type: String, 
+    required: true 
+  },
+  cost: { 
+    type: Number, 
+    required: true 
+  },
+  date: { 
+    type: Date, 
+    default: Date.now 
+  },
+});
+
+const Expense = mongoose.model("Expense", expenseSchema);
+module.exports = Expense;
