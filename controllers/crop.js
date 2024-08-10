68b220d9767be5e60d8142ae0e3d0bc33ecd0b96
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-10 13:34:14 +0530

    Added pesticide routes

--- controllers/crop.js
+++ controllers/crop.js
@@ -1,5 +1,6 @@
 const Crop = require("../models/Crop");
 const Farmer = require("../models/Farmer");
+const Pesticide = require("../models/Pesticide");
 
 const addCrop = async (req, res) => {
   const { farmerId } = req.params;
@@ -87,6 +88,7 @@ const deleteCrop = async (req, res) => {
 
 const getCrop = async (req, res) => {
   const { cropId } = req.params;
+
   try {
     const crop = await Crop.findById(cropId);
 
@@ -103,9 +105,115 @@ const getCrop = async (req, res) => {
   }
 };
 
+const addPesticide = async (req, res) => {
+  const { cropId } = req.params;
+  const { name, manufacturer, quantity, cost, description } = req.body;
+
+  try {
+    const crop = await Crop.findById(cropId);
+
+    if (!crop) {
+      return res.status(404).json({ message: "Crop not found for this farmer" });
+    }
+
+    const pesticide = new Pesticide({
+      name,
+      manufacturer,
+      quantity,
+      cost,
+      description,
+    });
+
+    await pesticide.save();
+
+    await Crop.findByIdAndUpdate(
+      cropId,
+      { $addToSet: { pesticides: pesticide._id } },
+      { new: true }
+    );
+
+    res.status(201).json({
+      message: "Pesticide added successfully",
+      pesticide,
+    });
+  } catch (error) {
+    console.error("Error adding pesticide:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const updatePesticide = async (req, res) => {
+  const { pesticideId } = req.params;
+  const { name, manufacturer, quantity, cost, description } = req.body;
+
+  try {
+    const pesticide = await Pesticide.findByIdAndUpdate(
+      pesticideId,
+      { name, manufacturer, quantity, cost, description },
+      { new: true, runValidators: true }
+    );
+
+    if (!pesticide) {
+      return res.status(404).json({ message: "Pesticide not found" });
+    }
+
+    res.status(200).json({
+      message: "Pesticide updated successfully",
+      pesticide,
+    });
+  } catch (error) {
+    console.error("Error updating pesticide:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const deletePesticide = async (req, res) => {
+  const { cropId, pesticideId } = req.params;
+	
+  try {
+    const pesticide = await Pesticide.findByIdAndDelete(pesticideId);
+
+    if (!pesticide) {
+      return res.status(404).json({ message: "Pesticide not found" });
+    }
+
+    await Crop.findByIdAndUpdate(
+			cropId, 
+			{ $pull: { pesticides: pesticide._id } }
+		);
+
+    res.status(200).json({
+      message: "Pesticide deleted successfully",
+    });
+  } catch (error) {
+    console.error("Error deleting pesticide:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+};
+
+const getPesticide = async (req, res) => {
+	const { pesticideId } = req.params;
+	try {
+		const pesticide = await Pesticide.findById(pesticideId);
+
+    if (!pesticide) {
+      return res.status(404).json({ message: "Pesticide not found" });
+    }
+
+    res.status(200).json(pesticide);
+	} catch (error) {
+		console.error("Error getting pesticide:", error);
+    res.status(500).json({ message: "Server error" });
+	}
+}
+
 module.exports = {
   addCrop,
   updateCrop,
   deleteCrop,
   getCrop,
+  addPesticide,
+  updatePesticide,
+  deletePesticide,
+	getPesticide
 };
--- routes/cropRoutes.js
+++ routes/cropRoutes.js
@@ -5,11 +5,19 @@ const {
   updateCrop,
   deleteCrop,
   getCrop,
+  addPesticide,
+  updatePesticide,
+  deletePesticide,
+  getPesticide,
 } = require('../controllers/crop');
 
 router.post('/:farmerId/add-crop', addCrop);
 router.put('/:cropId/update-crop', updateCrop);
 router.delete('/:farmerId/:cropId/delete-crop', deleteCrop);
 router.get('/:cropId/get-crop', getCrop);
+router.post('/:cropId/add-pesticide', addPesticide);
+router.put('/:pesticideId/update-pesticide', updatePesticide);
+router.delete('/:cropId/:pesticideId/delete-pesticide', deletePesticide);
+router.get('/:pesticideId/get-pesticide', getPesticide);
 
 module.exports = router;
