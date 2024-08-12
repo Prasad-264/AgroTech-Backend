ee8830a5dda9fd1298917cef3c9389c032de74ed
Prasad Kandekar <prasadkandekar555@gmail.com>
2024-08-12 20:04:23 +0530

    Added getAllFarmers routes

--- controllers/farmer.js
+++ controllers/farmer.js
@@ -98,9 +98,26 @@ const getFarmerById = async (req, res) => {
   }
 };
 
+const getAllFarmers = async (req, res) => {
+  const { userId } = req.params;
+  try {
+    const user = await User.findById(userId).populate("farmers");
+
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    res.status(200).json(user.farmers);
+  } catch (error) {
+    console.error("Error getting all farmer:", error);
+    res.status(500).json({ message: "Server error" });
+  }
+}
+
 module.exports = {
   addFarmer,
   updateFarmer,
   deleteFarmer,
   getFarmerById,
+  getAllFarmers
 };
--- routes/farmerRoutes.js
+++ routes/farmerRoutes.js
@@ -5,11 +5,12 @@ const {
   updateFarmer,
   deleteFarmer,
   getFarmerById,
+  getAllFarmers
 } = require('../controllers/farmer');
 
 router.post('/:userId/add-farmer', addFarmer);
 router.put('/:farmerId/update-farmer', updateFarmer);
 router.delete('/:userId/:farmerId/delete-farmer', deleteFarmer);
 router.get('/:farmerId/get-farmer', getFarmerById);
-
+router.get('/:userId/farmers', getAllFarmers);
 module.exports = router;
